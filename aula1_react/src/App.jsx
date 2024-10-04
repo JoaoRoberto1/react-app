228dd9103e788e83938b650439580f0ac9089f96
JOAO ROBERTO DE OLIVEIRA FELIX <ROBERTO6262@DIGITALCOLLEGE.LOCAL>
2024-10-03 21:55:14 -0300

    Subindo

--- aula1_react/src/App.jsx
+++ aula1_react/src/App.jsx
@@ -2,13 +2,26 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import Coleguinhas from './components/Coleguinhas';
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [nome, setNome] = useState('Edward');
+  const [idade, setIdade] = useState(30);
 
   return (
     <>
-      <h1>Roberto</h1>
+      {/* <h1>Nome: {nome}</h1>
+      <h2>Idade: {idade}</h2>
+      
+      <div className="card">
+        <button onClick={() => setNome('João')}>Mudar Nome</button>
+        <button onClick={() => setIdade('17')}>Mudar Idade</button>
+      </div> */}
+
+      <Coleguinhas nome={"Angelo"} nomeCompleto={"Angelo Lustosa de Figueireido Filho"} />
+      <Coleguinhas nome={"João"} nomeCompleto={"João Roberto de Oliveira Felix"} />
+      <Coleguinhas nome={"Edmar"} nomeCompleto={"Edmar Albuguerque Neto"} />
+      <Coleguinhas nome={"Gustavo"} nomeCompleto={"Luis Gustavo"} />
     </>
   )
 }
---
+++ aula1_react/src/components/Coleguinhas.jsx
@@ -0,0 +1,11 @@
+function Coleguinhas ({nome, nomeCompleto}) {
+
+    return (
+        <>
+            <h3>{nome}</h3>
+            <p>{nomeCompleto}</p>
+        </>
+    )
+}
+
+export default Coleguinhas;
